bed946197f25a4de28951431375dc1e90ab028ef
Lisanne Järv <lisannejarv@gmail.com>
2021-06-09 10:59:38 +0300

    ...

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-  define('db_host', 'localhost');
-  define('db_user', 'if20');
-  define('db_pass', 'if20');
-  define('db_name', 'if20_lisanne');
-?>
--- logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-	session_start();
-	session_unset($_SESSION['user_id']);
-	header('location:login.php');
-?>
